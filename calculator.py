3b582eb0cdf1fba0259d44c55a4dabc644c7e7c9
Mohd Kaif Ansari <155880242+techykaif@users.noreply.github.com>
2024-04-15 20:55:27 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+#This is the Second Task of the Python Programming in which i had to create Calculator which performs Operations Based on User's Choice
+#This Program is tested for Various Inputs.
+
 a=int(input("Enter the Number: "))
 c=str(input("Enter the Operand: "))
 b=int(input("Enter the Second Number: "))
